60a0ba61d747d6d4c1da8280463109ac7d70b732
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-03-17 18:18:51 -0300

    Add database config

--- src/api/server.js
+++ src/api/server.js
@@ -2,9 +2,10 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
-})
+});
 
-app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
+
+// const { DB_USER, DB_PASSWORD, DB_DATABASE, DB_HOST, DB_PORT } = process.env;
+
 module.exports = {
   development: {
-    username: 'root',
-    password: null,
-    database: 'database_development',
-    host: '127.0.0.1',
+    username: 'postgres',
+    password: 'sua_senha_aqui',
+    database: 'postgres',
+    host: 'db.cfrxekzacbqaicirfvnk.supabase.co',
+    port: 6543,
     dialect: 'postgres',
   },
   test: {
