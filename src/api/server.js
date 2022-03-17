20ff4a1d0b7dcae6dd3c8f34a938e64676bfb51e
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-03-17 17:53:37 -0300

    Alteração Pedro

--- src/api/server.js
+++ src/api/server.js
@@ -2,4 +2,9 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+})
+
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
